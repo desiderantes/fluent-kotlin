2bedf40d819267d0515d6c9f952fd79d20ff204a
Mario Daniel Ruiz Saavedra <desiderantes93@gmail.com>
2024-09-10 17:00:06 -0500

    Upgrade to Gradle 8.10

--- fluent.syntax/src/main/kotlin/org/projectfluent/syntax/visitor/Visitor.kt
+++ fluent.syntax/src/main/kotlin/org/projectfluent/syntax/visitor/Visitor.kt
@@ -1,6 +1,7 @@
 package org.projectfluent.syntax.visitor
 
 import org.projectfluent.syntax.ast.BaseNode
+import java.lang.reflect.Method
 import kotlin.reflect.KVisibility
 import kotlin.reflect.full.memberProperties
 
@@ -24,41 +25,41 @@ fun childrenOf(node: BaseNode) = sequence {
  * Implement public visitNodeType methods to add handling to AST node types,
  * e.g. `visitResource(node: Resource)` to handle the `Resource` node type.
  */
-//abstract class Visitor {
-//    private val handlers: MutableMap<String, Method> = mutableMapOf()
-//    init {
-//        this::class.java.declaredMethods.filter {
-//            it.name.startsWith("visit")
-//        }.map {
-//            handlers[it.name.substring("visit".length)] = it
-//        }
-//    }
-//
-//    /**
-//     * Primary entry point for all visitors.
-//     *
-//     * This is the method you want to call on concrete visitor implementations.
-//     */
-//    fun visit(node: BaseNode) {
-//        val cName = node::class.java.simpleName
-//        val handler = this.handlers[cName]
-//        if (handler != null) {
-//            handler.invoke(this, node)
-//        } else {
-//            this.genericVisit(node)
-//        }
-//    }
-//
-//    /**
-//     * From concrete `visitNodeType` implementations, call this
-//     * method to continue iteration into the AST if desired.
-//     */
-//    fun genericVisit(node: BaseNode) {
-//        childrenOf(node).map { (_, value) -> value }.forEach { value ->
-//            when (value) {
-//                is BaseNode -> this.visit(value)
-//                is Collection<*> -> value.filterIsInstance<BaseNode>().map { this.visit(it) }
-//            }
-//        }
-//    }
-//}
+abstract class Visitor {
+    private val handlers: MutableMap<String, Method> = mutableMapOf()
+    init {
+        this::class.java.declaredMethods.filter {
+            it.name.startsWith("visit")
+        }.map {
+            handlers[it.name.substring("visit".length)] = it
+        }
+    }
+
+    /**
+     * Primary entry point for all visitors.
+     *
+     * This is the method you want to call on concrete visitor implementations.
+     */
+    fun visit(node: BaseNode) {
+        val cName = node::class.java.simpleName
+        val handler = this.handlers[cName]
+        if (handler != null) {
+            handler.invoke(this, node)
+        } else {
+            this.genericVisit(node)
+        }
+    }
+
+    /**
+     * From concrete `visitNodeType` implementations, call this
+     * method to continue iteration into the AST if desired.
+     */
+    fun genericVisit(node: BaseNode) {
+        childrenOf(node).map { (_, value) -> value }.forEach { value ->
+            when (value) {
+                is BaseNode -> this.visit(value)
+                is Collection<*> -> value.filterIsInstance<BaseNode>().map { this.visit(it) }
+            }
+        }
+    }
+}
--- fluent.syntax/src/test/java/org/projectfluent/testing/JavaWordCounter.java
+++ fluent.syntax/src/test/java/org/projectfluent/testing/JavaWordCounter.java
@@ -1,30 +1,29 @@
-//package org.projectfluent.testing;
-//
-//import org.projectfluent.syntax.ast.Resource;
-//import org.projectfluent.syntax.ast.TextElement;
-//import org.projectfluent.syntax.visitor.Visitor;
-//
-//import java.util.regex.Pattern;
-//
-//public class JavaWordCounter extends Visitor {
-//    private static final java.util.regex.Pattern WORD_BOUNDARY = Pattern.compile("\\W+");
-//
-//    private int words;
-//    public JavaWordCounter() {
-//        words = 0;
-//    }
-//
-//    public int getWords() {
-//        return words;
-//    }
-//
-//    public void visitResource(Resource node) {
-//        System.out.println("resource");
-//        this.genericVisit(node);
-//    }
-//
-//    public void visitTextElement(TextElement node) {
-//        String val = node.getValue();
-//        words += WORD_BOUNDARY.split(val).length;
-//    }
-//}
+package org.projectfluent.testing;
+
+import org.projectfluent.syntax.ast.BaseNode;
+import org.projectfluent.syntax.visitor.Visitor;
+
+import java.util.regex.Pattern;
+
+public class JavaWordCounter extends Visitor {
+    private static final java.util.regex.Pattern WORD_BOUNDARY = Pattern.compile("\\W+");
+
+    private int words;
+    public JavaWordCounter() {
+        words = 0;
+    }
+
+    public int getWords() {
+        return words;
+    }
+
+    public void visitResource(BaseNode.SyntaxNode.Resource node) {
+        System.out.println("resource");
+        this.genericVisit(node);
+    }
+
+    public void visitTextElement(BaseNode.SyntaxNode.PatternElement.TextElement node) {
+        String val = node.getValue();
+        words += WORD_BOUNDARY.split(val).length;
+    }
+}
